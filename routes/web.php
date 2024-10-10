0760c18c0964d308eed33a81c66bb8984051f18b
Laura de Oliveira <l4ur4.88@gmail.com>
2024-10-10 18:10:00 -0300

    Add projects views with Livewire

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'projects.index')->name('projects.index');
+
+Route::view('/projects/{project}', 'projects.show')->name('projects.show');
