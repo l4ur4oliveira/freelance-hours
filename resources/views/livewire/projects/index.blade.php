b0a00b818b248942d9b52cba6504d215bb19df13
Laura de Oliveira <l4ur4.88@gmail.com>
2024-10-10 23:02:13 -0300

    Show tech icons

--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-2 gap-4">
     @foreach($this->projects as $project)
     <a href="{{ route('projects.show', $project) }}">
-        <x-project-card-mini :$project />
+        <x-projects.card-mini :$project />
     </a>
     @endforeach
 </div>
