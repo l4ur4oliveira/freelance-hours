fff963756577fe46308f17b2152cfaa13c9b1734
Laura de Oliveira <l4ur4.88@gmail.com>
2025-02-13 21:18:09 -0300

    Add dynamic data to proposals components

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,39 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
 
+    public int $quantity = 5;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()
+        ->orderBy('hours')
+        ->paginate($this->quantity);
+    }
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()
+            ->latest()
+            ->first()
+            ->created_at->diffForHumans();
+    }
+
+    public function loadMore()
+    {
+        $this->quantity += 5;
+    }
+
+    #[On('proposal::created')]
     public function render()
     {
         return view('livewire.projects.proposals');
